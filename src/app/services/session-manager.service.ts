6cb8d22b9aeeb82f179d8ba694fcc57f6fbca9b9
Madhan-Balaji <madhanb69@gmail.com>
2017-12-07 18:51:55 +0530

    service modification

--- src/app/services/session-manager.service.ts
+++ src/app/services/session-manager.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { UserDetail } from '../models/user-detail';
+
 @Injectable()
 export class SessionManagerService {
   private baseUrl = 'http://localhost:8080/carshop/Jserv/';
@@ -19,5 +21,16 @@ export class SessionManagerService {
     bodyHard.set('pwd', password);
     const body = bodyHard.toString();
     return this.http.post(this.signinUrl, body, { headers: new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded') });
+  }
+
+  signupCall(newUser: UserDetail) {
+    const bodyHard = new URLSearchParams();
+    bodyHard.set('name', newUser.name);
+    bodyHard.set('pwd', newUser.password);
+    bodyHard.set('email', newUser.email);
+    bodyHard.set('region', newUser.region);
+    bodyHard.set('phone', newUser.phone.toString());
+    const body = bodyHard.toString();
+    return this.http.post(this.signupUrl, body, { headers: new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded') });
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserDetail } from '../models/user-detail';
+import { SessionManagerService } from '../services/session-manager.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  newUser: UserDetail;
+  rpwd: any;
+  constructor(private sessionManager: SessionManagerService) { }
 
   ngOnInit() {
+  }
+
+  signupSubmit() {
+    this.sessionManager.signupCall(this.newUser).subscribe(
+      response => console.log(response)
+    );
   }
 
 }
