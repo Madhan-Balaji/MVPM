6759fa8918a937a2b139b9e720343726404380d3
Madhan-Balaji <madhanb69@gmail.com>
2017-12-07 18:04:49 +0530

    machine 2 edit

--- src/app/services/session-manager.service.ts
+++ src/app/services/session-manager.service.ts
@@ -3,20 +3,21 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class SessionManagerService {
-  private baseUrl = "http://localhost:8080/carshop/Jserv/";
-  private serviceSpecificUrl = this.baseUrl + "control/";
+  private baseUrl = 'http://localhost:8080/carshop/Jserv/';
+  private serviceSpecificUrl = this.baseUrl + 'control/';
 
-  private signinUrl = this.serviceSpecificUrl + "userLogin";
-  private signupUrl = this.serviceSpecificUrl + "newUser";
+  private signinUrl = this.serviceSpecificUrl + 'userLogin';
+  private signupUrl = this.serviceSpecificUrl + 'newUser';
 
   constructor(
     private http: HttpClient
   ) { }
 
   signinCall(email, password) {
-    let body = new URLSearchParams();
-    body.set("mail", email);
-    body.set("pwd", password);
-    return this.http.post(this.signinUrl, body.toString(), { headers: new HttpHeaders().set("Content-type", "application/x-www-form-urlencoded") });
+    const bodyHard = new URLSearchParams();
+    bodyHard.set('mail', email);
+    bodyHard.set('pwd', password);
+    const body = bodyHard.toString();
+    return this.http.post(this.signinUrl, body, { headers: new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded') });
   }
 }
